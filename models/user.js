ba8bc631c66bfdc7f92173dc8a41448b9d544259
Muhammad Tahir <mtahir.ps@gmail.com>
2019-10-30 00:09:22 +0500

    user schema

---
+++ models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const eventSchema = new Schema({
+    email: {
+        type: String,
+        required: true
+    },
+    password: {
+        type: String,
+        required: true
+
+    },
+    createdEvents: [{
+        type: Schema.Types.ObjectId,
+        ref: 'Event'
+    }],
+})
+
+module.exports = mongoose.model("User", eventSchema);
