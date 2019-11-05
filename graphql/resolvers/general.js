4731db8b388d6769d9b43b5eeeedb8ae87b71645
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-05 20:45:03 +0500

    create general functions

---
+++ graphql/resolvers/general.js
@@ -0,0 +1,72 @@
+const Event = require('./../../models/event');
+const User = require('./../../models/user');
+const { dateToString } = require('./../../helpers/date');
+
+const events = async eventIds => {
+    try {
+        const events = await Event.find({ _id: { $in: eventIds } })
+        return events.map(event => {
+            return {
+                ...event._doc,
+                _id: event.id,
+                date: dateToString(event._doc.date),
+                creator: user.bind(this, event.creator)
+            }
+        })
+    } catch (error) {
+        throw error
+    }
+}
+const singleEvent = async eventId => {
+    try {
+        const event = await Event.findById(eventId)
+        return transformEvent(event)
+    } catch (error) {
+        throw error
+    }
+}
+
+const user = async userId => {
+    try {
+        const user = await User.findById(userId)
+        return transformUser(user)
+    } catch (error) {
+        throw error
+    }
+}
+
+const transformBooking = (booking) => {
+    return {
+        ...booking._doc,
+        _id: booking.id,
+        user: user.bind(this, booking._doc.user),
+        event: singleEvent.bind(this, booking._doc.event),
+        createdAt: dateToString(booking._doc.createdAt),
+        updatedAt: dateToString(booking._doc.updatedAt),
+    }
+}
+
+const transformEvent = (event) => {
+    return {
+        ...event._doc,
+        _id: event.id,
+        date: dateToString(event._doc.date),
+        creator: user.bind(this, event._doc.creator)
+    }
+}
+
+const transformUser = (user) => {
+    return {
+        ...user._doc,
+        _id: user.id,
+        createdEvents: events.bind(this, user._doc.createdEvents)
+    }
+}
+
+
+exports.events = events
+exports.singleEvent = singleEvent
+exports.transformBooking = transformBooking
+exports.transformEvent = transformEvent
+exports.transformUser = transformUser
+exports.user = user
