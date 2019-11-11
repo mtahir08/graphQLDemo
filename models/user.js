4aa060fc57ff8268d734dbec34fc21f23bbfb950
Muhammad Tahir <mtahir.ps@gmail.com>
2019-11-12 00:26:32 +0500

    fix typo

--- models/user.js
+++ models/user.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
-const eventSchema = new Schema({
+const userSchema = new Schema({
     email: {
         type: String,
         required: true
@@ -17,4 +17,4 @@ const eventSchema = new Schema({
     }],
 })
 
-module.exports = mongoose.model("User", eventSchema);
+module.exports = mongoose.model("User", userSchema);
