ac34769aa7162808e8c0eeb064af76d51cb97871
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-29 21:14:38 +0500

    add context consumer

--- GraphQL-React-Event-Book-Example/frontend/src/components/Navigation/MainNavigation.js
+++ GraphQL-React-Event-Book-Example/frontend/src/components/Navigation/MainNavigation.js
@@ -2,25 +2,33 @@ import React from 'react';
 import { NavLink } from 'react-router-dom'
 
 import './MainNavigation.css'
+import { AuthContext } from './../../context/auth-context'
 const MainNavigation = (props) => (
-    <header className='main-navigation'>
-        <div className='main-navigation-logo'>
-            <h1>Easy Events</h1>
-        </div>
-        <div className='main-navigation-item'>
-            <ul>
-                <li>
-                    <NavLink to='/auth'>Authentication</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/events'>Events</NavLink>
-                </li>
-                <li>
-                    <NavLink to='/bookings'>Bookings</NavLink>
-                </li>
-            </ul>
-        </div>
-    </header>
+    <AuthContext.Consumer>
+        {(context) => (
+            <header className='main-navigation'>
+                <div className='main-navigation-logo'>
+                    <h1>Easy Events</h1>
+                </div>
+                <div className='main-navigation-item'>
+                    <ul>
+                        {!context.token && <li>
+                            <NavLink to='/auth'>Authentication</NavLink>
+                        </li>
+                        }
+                        <li>
+                            <NavLink to='/events'>Events</NavLink>
+                        </li>
+                        {
+                            context.token && <li>
+                                <NavLink to='/bookings'>Bookings</NavLink>
+                            </li>
+                        }
+                    </ul>
+                </div>
+            </header>
+        )}
+    </AuthContext.Consumer>
 );
 
 export { MainNavigation };
