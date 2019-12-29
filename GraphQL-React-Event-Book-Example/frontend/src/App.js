71d0e86185415b525b94cc1092f90451be3de9ff
Muhammad Tahir <mtahir.ps@gmail.com>
2019-12-29 21:14:27 +0500

    add context provider

--- GraphQL-React-Event-Book-Example/frontend/src/App.js
+++ GraphQL-React-Event-Book-Example/frontend/src/App.js
@@ -1,21 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom'
 
 import { Auth, Bookings, Events } from "./pages";
 import { MainNavigation } from './components/Navigation/MainNavigation'
+import { AuthContext } from './context/auth-context'
 
-function App() {
-  return (
-    <BrowserRouter>
-      <MainNavigation />
-      <Switch>
-        <Redirect from='/' to='/auth' exact />
-        <Route path='/auth' component={Auth} />
-        <Route path='/events' component={Events} />
-        <Route path='/bookings' component={Bookings} />
-      </Switch>
-    </BrowserRouter>
-  );
+class App extends Component {
+  state = {
+    token: null,
+    userId: null
+  }
+
+  login = (token, userId, tokenExpiration) => {
+    this.setState({ token, userId });
+  }
+
+  logout = (token, userId, tokenExpiration) => {
+    this.setState({ token: null, userId: null });
+  }
+
+  render() {
+    return (
+      <BrowserRouter>
+        <AuthContext.Provider value={{
+          token: this.state.token,
+          userId: this.state.userId,
+          login: this.login,
+          logout: this.logout,
+        }}>
+          <MainNavigation />
+          <Switch>
+            <Redirect from='/' to='/auth' exact />
+            <Route path='/auth' component={Auth} />
+            <Route path='/events' component={Events} />
+            <Route path='/bookings' component={Bookings} />
+          </Switch>
+        </AuthContext.Provider>
+      </BrowserRouter >
+    );
+  }
 }
 
 export default App;
--- GraphQL-React-Event-Book-Example/frontend/src/pages/Auth.js
+++ GraphQL-React-Event-Book-Example/frontend/src/pages/Auth.js
@@ -2,6 +2,8 @@ import React, { Component } from 'react';
 import Form from 'react-bootstrap/Form'
 import Button from 'react-bootstrap/Button'
 import Container from 'react-bootstrap/Container'
+
+import { AuthContext } from './../context/auth-context'
 class Auth extends Component {
     constructor(props) {
         super(props);
@@ -11,6 +13,7 @@ class Auth extends Component {
             isLogin: false
         }
     }
+    static contextType = AuthContext
 
     onClick = () => {
         this.setState({ isLogin: !this.state.isLogin })
@@ -60,7 +63,9 @@ class Auth extends Component {
                 return response.json()
             })
             .then((json) => {
-                console.log(json)
+                if (json.data.login && json.data.login.token) {
+                    this.context.login(json.data.login.token, json.data.login.userId)
+                }
             })
             .catch((error) => {
                 console.log(error)
