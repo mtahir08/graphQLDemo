ceb9b46db54b577d86021d67e315ea3530e7b09d
Muhammad Tahir <mtahir.ps@gmail.com>
2020-01-04 22:20:20 +0500

    Revert "add condition for chart"
    This reverts commit ce2dfac93af60ca08cbd07f50fb6854d3e0f8205.

--- GraphQL-React-Event-Book-Example/frontend/src/pages/Bookings.js
+++ GraphQL-React-Event-Book-Example/frontend/src/pages/Bookings.js
@@ -2,16 +2,13 @@ import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button'
 import ListGroup from 'react-bootstrap/ListGroup'
 import Spinner from 'react-bootstrap/Spinner'
-import ToggleButtonGroup from 'react-bootstrap/ToggleButtonGroup'
-import ToggleButton from 'react-bootstrap/ToggleButton'
 
 import { AuthContext } from './../context/auth-context'
 
 class Bookings extends Component {
     state = {
         bookings: [],
-        loading: false,
-        type: "1"
+        loading: false
     }
 
     static contextType = AuthContext;
@@ -114,47 +111,22 @@ class Bookings extends Component {
             })
     }
 
-    handleChange = (checkedItem) => {
-        this.setState({ type: checkedItem })
-    }
-
     render() {
-        const { type, bookings } = this.state;
         if (this.state.loading)
             return <div style={{ textAlign: 'center' }}>
                 <Spinner animation="grow" />
             </div>
-
-        const content = <React.Fragment>
-            {type === '1' ?
-                <ListGroup>
-                    {bookings.map((item, index) => (
-                        <ListGroup.Item eventKey={index.toString()} style={{ display: 'flex', justifyContent: 'space-around' }}>
-                            {item.event.title}
-                            <Button onClick={() => this.cancelBooking(item._id)}>
-                                Cancel
-                            </Button>
-                        </ListGroup.Item>
-                    ))}
-                </ListGroup>
-                :
-                <div>Chart</div>
-            }
-
-        </React.Fragment>
-
         return (
-            <React.Fragment>
-                <ToggleButtonGroup toggle name="options" onChange={this.handleChange} value={type}>
-                    <ToggleButton type="radio" name="radio" defaultChecked value="1">
-                        List
-                    </ToggleButton>
-                    <ToggleButton type="radio" name="radio" value="2">
-                        Chart
-                    </ToggleButton>
-                </ToggleButtonGroup>
-                {content}
-            </React.Fragment>);
+            <ListGroup>
+                {this.state.bookings.map((item, index) => (
+                    <ListGroup.Item eventKey={index.toString()} style={{ display: 'flex', justifyContent: 'space-around' }}>
+                        {item.event.title}
+                        <Button onClick={() => this.cancelBooking(item._id)}>
+                            Cancel
+                    </Button>
+                    </ListGroup.Item>
+                ))}
+            </ListGroup>);
 
     }
 }
